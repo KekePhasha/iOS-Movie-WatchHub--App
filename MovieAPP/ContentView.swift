6a0917d5154df7c196d97a44df8b04b93b1edd60
Kekeletso Phasha <kekephasha@gmail.com>
2023-06-26 10:01:18 +0200

    Initial Commit

---
+++ MovieAPP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieAPP
+//
+//  Created by Kekeletso Phasha on 2023/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieAPP/MovieAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAPPApp.swift
+//  MovieAPP
+//
+//  Created by Kekeletso Phasha on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
